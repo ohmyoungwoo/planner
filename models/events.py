2734b6fb4471c078d097e856fdf9459c9f6fe905
ohmyoungwoo <myoungou.oh@lge.com>
2024-03-04 15:17:14 +0900

    SQL dB 활용한 CRUD

--- models/events.py
+++ models/events.py
@@ -1,21 +1,43 @@
-from pydantic import BaseModel
-from typing import List
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import Optional, List
 
-class Event(BaseModel):
-    id: int
+#from pydantic import BaseModel
+#from typing import List
+
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key= True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
     
     class Config:
+        arbitrary_types_allowed = True
         schema_extra = {
-            "example": {
-                "title": "FastAPI Book Launch",
+            "example":{
+                "title": "FastAPI Book 출시",
                 "image": "https://linktomyimage.com/image.png",
-                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
-                "tags": ['python', 'fastapi', 'book', 'launch'],
+                "description": "We will be discussing the contents of the FastAPI book in this event.",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+    
+    class Config:
+        schema_extra = {
+            "example":{
+                "title": "FastAPI Book 출시",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event.",
+                "tags": ["python", "fastapi", "book", "launch"],
                 "location": "Google Meet"
             }
         }
--- routes/events.py
+++ routes/events.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter, Body, HTTPException, status
-from models.events import Event
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from sqlmodel import select
+
+from database.connection import get_session
+from models.events import Event, EventUpdate
+
 from typing import List
 
 # 이벤트 라우트 정의 (Chaptor 5 p100의 1)
@@ -9,17 +13,20 @@ event_router = APIRouter(
 
 events = []
 
-# 모든 이벤트 추출  (Chaptor 5의 p100의 2)
+# 모든 이벤트 추출 in SQL dB  (Chaptor 6의 p115)
 @event_router.get("/", response_model = List[Event])
-async def retrieve_all_events() -> List[Event]:
+async def retrieve_all_events(session=Depends(get_session)):
+    statement = select(Event)
+    events = session.exec(statement).all()
+    
     return events
 
-# 특정 ID의 이벤트 추출   (Chaptor 5의 p100의 2)
+# 모든 이벤트 추출 in SQL dB  (Chaptor 6의 p115)
 @event_router.get("/{id}", response_model=Event)
-async def retrieve_event(id: int) -> Event:
-    for event in events:
-        if event.id == id:
-            return event
+async def retrieve_event(id: int, session=Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+    if event:
+        return event
     
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
@@ -27,10 +34,13 @@ async def retrieve_event(id: int) -> Event:
     )
     
 
-# 이벤트 생성 및 삭제 라우트 정의 (Chaptor 5의 p101의 3)
+# 이벤트 생성 및 삭제 라우트 정의 SQL dB에서 (Chaptor 6의 p114)
 @event_router.post("/new")
-async def create_event(body: Event = Body(...)) -> dict:
-    events.append(body)
+async def create_event(new_event: Event, session=Depends(get_session)) -> dict:
+    session.add(new_event)
+    session.commit()
+    session.refresh(new_event)
+
     return {
         "message": "Event created successfully."
     }
