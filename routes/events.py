f0737c6a880439f201a1e2f9911ff810a9f4269b
ohmyoungwoo <myoungou.oh@lge.com>
2024-03-04 16:08:37 +0900

    SQL dB 활용한 CRUD 2

--- routes/events.py
+++ routes/events.py
@@ -46,17 +46,19 @@ async def create_event(new_event: Event, session=Depends(get_session)) -> dict:
     }
     
 @event_router.delete("/{id}")
-async def delete_event(id: int):
-    for event in events:
-        if event.id == id:
-            events.remove(event)
-            return {
-                "message": "Event deleted successfully."
+async def delete_event(id: int, session=Depends(get_session)):
+    event = session.get(Event, id)
+    if event:
+        session.delete(event)
+        session.commit()
+        return {
+            "message": "Event deleted successfully."
             }
-        raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND,
-            detail="Event with supplied ID does not exist."
-        )
+        
+    raise HTTPException(
+        status_code = status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist."
+    )
         
 @event_router.delete("/")
 async def delete_all_events() -> dict:
@@ -64,3 +66,24 @@ async def delete_all_events() -> dict:
     return {
         "message": "Events deleted successfully."
     }
+    
+# SQL dB 활용한 Table Update
+@event_router.put("/edit/{id}", response_model=Event)
+async def update_event(id: int, new_data: EventUpdate, session=Depends(get_session)):
+    event = session.get(Event, id)
+    if event:
+        event_data = new_data.dict(exclude_unset=True)
+        
+        for key, value, in event_data.items():
+            setattr(event, key, value)
+            
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+        
+        return event
+    
+    raise HTTPException(
+        status_code = status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist."
+    )
